a41d2fbb1841c65c623525fe82de37dc6e232d17
nngw <thanushiyan@hotmail.co.uk>
2023-05-26 10:06:28 +0100

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     Everyday I'm learning
+    </>
+  )
+}
+
+export default App
