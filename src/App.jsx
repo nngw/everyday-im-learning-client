b4d3dec978fb5e8506695d99c04933b82a82a16d
Sean <sean_obeirne@outlook.com>
2023-05-30 17:12:52 +0100

    Added TaskForm, Added to Proflie Page to use the TaskFrom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import * as Pages from './pages'
-import { NavBar } from './componants'
+import { NavBar, TaskForm } from './componants'
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
           <Route path="/login" element={<Pages.Login />}/>
           <Route path="/register" element={<Pages.Register/>}/>
           <Route path="/focus" element={<Pages.Focus/>}/>
-          <Route path="/tasks" element={<Pages.Tasks/>}/>
+          <Route path="/profile" element={<Pages.Profile/>}/>
           <Route path="/NotFound" element={<Pages.NotFound/>}/>
         </Route>
       </Routes>
