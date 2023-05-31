00be0be544efbb86906d05c9ee99721501d74c46
nngw <thanushiyan@hotmail.co.uk>
2023-05-31 14:13:46 +0100

    connecting to backend for getting tasks and posting

--- src/components/TaskForm/index.jsx
+++ src/components/TaskForm/index.jsx
@@ -1,26 +1,73 @@
 import React,{useState} from "react";
+
+import { useTasksContext } from "../../hooks/useTasksConext";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 import './style.css'
 
 function TaskForm () {
+    const {dispatch} = useTasksContext()
+    const {user} = useAuthContext()
+
     const [inputText, setInputText] = useState('');
+    const [duration, setDuration] = useState('')
+    // const [error,setError] = useState(null)
+    // const [emptyFields, setEmptyFields] = useState([])
+    
     function handleInput(e) {
         setInputText(e.target.value)
     }
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        setTasks([
-            ...tasks,
-            {text: inputText, complted: false}
-        ])
-        setInputText('')
+        // setTasks([
+        //     ...tasks,
+        //     {text: inputText, complted: false}
+        // ])
+        // setInputText('')
+
+        const task = {inputText, duration}
+
+        const res = await fetch('http://localhost:9000/tasks', {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+
+        const json = await res.json()
+
+        console.log(user.token)
+
+        if (res.ok) {
+            setDuration("")
+            setInputText("")
+            console.log("new task added", json)
+            dispatch({type: 'CREATE_TASK', payload: json})
+        }
+
     }
     
     return (
         <form className ='TaskForm' onSubmit={handleSubmit}>
-            <input value={inputText} type="text" className="task-input"
-            onChange={handleInput}/>
-            <button type="submit" className="task-button">Add Task</button>
+            <label>Tasks</label>
+            <input 
+                value={inputText} 
+                type="text" 
+                className="task-input"
+                onChange={handleInput} 
+                aria-label="Input tasks"/>
+
+            <label>Task duration (min)</label>
+            <input 
+                type="number" 
+                onChange={(e) => setDuration(e.target.value)}
+                value={duration}/>
+        
+
+            <button type="submit" className="task-button" aria-label="Submit tasks">Add Task</button>
         </form>
     )
 }
