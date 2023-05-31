79f44c43b258b692f4955a334386e4bc7ed084ee
Sean <sean_obeirne@outlook.com>
2023-05-31 12:01:29 +0100

    Example styling applied and edited to match theme

--- src/components/WeeklyCalendar/index.jsx
+++ src/components/WeeklyCalendar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import './style.css';
 
 
 const WeeklyCalendar = () => {
