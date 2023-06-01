d6a83e96a92a46198ebc8d2d8d4d2085e738dd29
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-06-01 09:19:52 +0100

    added del bin

--- src/components/TaskItem/index.jsx
+++ src/components/TaskItem/index.jsx
@@ -4,17 +4,18 @@ import './style.css'
 const TaskItem = ({task,provided}) => {
   return (
     <>
-      <li className="task" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+      <ul className="task" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
         <span className="task-item">{task}</span>
         <button aria-label='delete task button' className="delete-task-btn"><span className="material-symbols-outlined">delete</span></button>
         {/* <button aria-label = 'delete task' className="trash-btn"
           onClick ={()=> deleteTask(task)}>&#128465;</button> */}
-      </li>
-      <li className="break">
-      <span className="break-item">Break</span>
-      {/* <button aria-label = 'delete task' className="trash-btn"
-        onClick ={()=> deleteTask(task)}>&#128465;</button> */}
-      </li>
+          <li className="break" >
+            <span className="break-item">Break</span>
+            {/* <button aria-label = 'delete task' className="trash-btn"
+              onClick ={()=> deleteTask(task)}>&#128465;</button> */}
+          </li>
+      </ul>
+      
     </>
    
   )
