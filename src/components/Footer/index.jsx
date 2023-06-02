33b12390d6c7615d00e84ab4f6c4b7d684b11502
angela2264 <angela2264@hotmail.com>
2023-06-02 09:47:30 +0100

    footer complete

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,9 +3,9 @@ import './index.css'
 
 const Footer = () => {
   return (
-    <div className ="footer"> 
+    <footer className ="footer"> 
         <p>Click <a href="https://wwf.panda.org/discover/knowledge_hub/endangered_species/giant_panda/whatyoucando/">here</a> to find out more information to protect the pandas.</p>
-    </div>
+    </footer>
   )
 }
 
